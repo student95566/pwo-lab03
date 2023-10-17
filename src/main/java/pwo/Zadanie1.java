8b612a3a088c0a28f8f0c52e69c92f1043114acb
Konrad Skolimowski <s95566@pollub.edu.pl>
2023-10-17 16:46:53 +0200

    Wykonanie Zadania 2 - wersja 1

--- src/main/java/pwo/Zadanie1.java
+++ src/main/java/pwo/Zadanie1.java
@@ -4,6 +4,9 @@ public class Zadanie1 {
 
     public static final int FROM = 0, TO = 100;
     public static final String FILE_NAME = "fibseq100.txt";
+    
+    public static final int FROM2 = 0, TO2 = 100;
+    public static final String FILE_NAME2 = "lucas100.txt";
 
     public static void main(String[] args) {
 
@@ -19,5 +22,17 @@ public class Zadanie1 {
             System.out.println("Błąd");
         }
 
+        System.out.println("lucas wersja 1");
+
+        boolean ok2 = SequenceTools2.writeToFile(
+                new LucasGenerator(), FROM2, TO2, FILE_NAME2);
+
+        if (ok2) {
+            System.out.println("Wynik zapisany do pliku: "
+                    + FILE_NAME);
+        } else {
+            System.out.println("Błąd");
+        }
+    
     }
 }
